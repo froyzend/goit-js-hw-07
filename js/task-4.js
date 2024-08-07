1d02f2a6c3f4025a42f564f08033e23d362e6775
Froyzend <froyzend1993@gmail.com>
2024-08-07 20:42:19 +0200

    ADD

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
+document.querySelector('.login-form').addEventListener('submit', function handleSubmit(event) {
+    event.preventDefault();
+  const formElements = this.elements;
+  let allFieldsFilled = true;
+  let formData = {};
 
+    for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+        if (element.type === 'submit' || element.type === 'button') {
+            continue;
+        }
+        if (element.value.trim() === '') {
+            allFieldsFilled = false;
+            break;
+      }
+      formData[element.name] = element.value.trim();
+    }
+
+    if (!allFieldsFilled) {
+        alert('All form fields must be filled in.');
+        
+    } else {
+       console.log(formData);
+        this.reset();
+    }
+});
